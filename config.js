83ac115f3faeb03c7722d4873ef9d116bb46f6ed
thsgw2020 <thsgw2020@gmail.com>
2021-05-23 22:38:30 +0900

    fix config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://thsgw2020.net/',
   pathPrefix: '/',
   title: 'thsgw2020 Blog',
   subtitle: 'Cloud & Automation',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
